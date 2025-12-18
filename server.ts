c6cfed00e174a04d2b818c5436dc99a1eaaf948d
NDOPING WILSON FOMBASOH <wilsonndoping@gmail.com>
2025-12-18 06:01:35 +0100

    fuck you

--- server.ts
+++ server.ts
@@ -20,11 +20,17 @@ c();
 // Middleware
 app.use(helmet());
 app.use(cors({
-  origin: ["http://localhost:5173","http://localhost:5174","dashboard-saas-git-master-wilideparisdenodes-projects.vercel.app"],
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://dashboard-saas-git-master-wilideparisdenodes-projects.vercel.app",
+    "https://dashboard-saas-h26gy72m5-wilideparisdenodes-projects.vercel.app"
+  ],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
+
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
